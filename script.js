3573812f9c5b29cea82d4c42ef0c0993da91366b
Tahmeerpasha <tahmeerpashacjc@gmail.com>
2023-04-09 20:02:30 +0530

    Calculator

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 let string = ""
 let buttons = document.querySelectorAll('.button');
 
-Array.from(buttons).forEach((button)=>{
-    button.addEventListener('click', (event)=>{
-        if(event.target.innerHTML == '='){
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (event) => {
+        if (event.target.innerHTML == '=') {
             string = eval(string)
 
-        }else{
-        string += event.target.innerHTML;
+        } else {
+            string += event.target.innerHTML;
         }
         document.querySelector('input').value = string
     })
 })
 
 let deletes = document.querySelector('.delete')
-    deletes.addEventListener('click',(e)=>{
-        string = string.substring(0,string.length-1)
-        document.querySelector('input').value = string
-    })
+deletes.addEventListener('click', (e) => {
+    string = string.substring(0, string.length - 1)
+    document.querySelector('input').value = string
+})
 
 let clear = document.querySelector('.clear')
-    clear.addEventListener('click',(e)=>{
-        string = "";
-        document.querySelector('input').value = string
-    })
+clear.addEventListener('click', (e) => {
+    string = "";
+    document.querySelector('input').value = string
+})
